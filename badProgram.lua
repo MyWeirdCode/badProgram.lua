52a19fa76906c0d27da3fbe5cdef223b506ad230
MyWeirdCode <102192107+MyWeirdCode@users.noreply.github.com>
2022-07-02 12:13:41 -0400

    Update badProgram.lua
    e

--- badProgram.lua
+++ badProgram.lua
@@ -1,4 +1,4 @@
-io.write("what programming languages do you use?\n") language = io.read() io.write("ok\n")
+io.write("what programming languages do you use?\n") language = io.read() io.write("ok\n") io.write("So you use ") io.write(language)
 --[[
   lua:
   https://www.lua.org/images/lua-logo.gif
